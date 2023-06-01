18c6370dcfd0617876e837095c24104232ee1905
Xientra <articbuisness@gmail.com>
2023-06-01 22:31:09 +0100

    feat: get cdn url from hash

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+def getCdnUrlFromHash(hash: str) -> str:
+    st = 31
+    for i in range(0, 32):
+        st ^= ord(hash[i])
+    return f"https://t{st % 8}.rbxcdn.com/{hash}"
