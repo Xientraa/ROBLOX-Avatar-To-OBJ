92c79b0935fe21cbdca75a3bfecbcde597582ed6
Xientra <articbuisness@gmail.com>
2023-06-04 23:23:21 +0100

    feat: add inputs for avatar type & avatar scales

--- src/main.py
+++ src/main.py
@@ -242,16 +242,52 @@ def downloadAvatarFromUserId(
     )
 
 
+def isStringInt(string: str) -> bool:
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+
+def avatarTypeFlow() -> Literal["R6", "R15"]:
+    print("Select an avatar type: 1) R6 2) R15")
+    while True:
+        inputString = input(">> ")
+        if isStringInt(inputString) and int(inputString) in [1, 2]:
+            return "R6" if inputString == "1" else "R15"
+        else:
+            print("[ERROR] Please make sure input is a valid number")
+
+
+def userIdFlow() -> int:
+    print("Enter a ROBLOX user id.")
+    while True:
+        inputString = input(">> ")
+        if isStringInt(inputString):
+            return int(inputString)
+        else:
+            print("[ERROR] Please make sure input is a valid number")
+
+
+def useAvatarScalesFlow() -> bool:
+    print("Use avatar scales (y/n)")
+    while True:
+        inputString = input(">> ").lower()
+        if inputString in ["y", "n"]:
+            return True if inputString == "y" else False
+        else:
+            print("[ERROR] Please make sure input is either y or n")
+
+
 if __name__ == "__main__":
     cookie = RobloSecurityCookie(CONFIG["Cookie"])
 
     while True:
-        inputString = input("UserID >> ")
-        try:
-            userId = int(inputString)
-            print(f"[INFO] Downloading userId: {userId}")
-            downloadAvatarFromUserId(
-                userId, CONFIG["DownloadDirectory"], cookie, "R6", True
-            )
-        except ValueError:
-            print("[ERROR] Please make sure input is a valid number")
+        userId = userIdFlow()
+        avatarType = avatarTypeFlow()
+        useAvatarScales = useAvatarScalesFlow()
+        print(f"[INFO] Downloading userId: {userId}")
+        downloadAvatarFromUserId(
+            userId, CONFIG["DownloadDirectory"], cookie, avatarType, useAvatarScales
+        )
